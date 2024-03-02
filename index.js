c4a2222b1f7bb7a70d79f19c47239b82277404fe
Renata1973 <157941623+Renata1973@users.noreply.github.com>
2024-03-02 10:39:47 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("Hello World!");
+var greenbutton=document.getElementById("green");
+if (greenbutton){
+    greenbutton.addEventListener('click', calAllert);
+    greenbutton.addEventListener('mouseover', banana);
+    greenbutton.addEventListener('mouseout', orange);
+
+}
+
+function calAllert (){
+    alert ("Call me")
+}
+
+//console.log(greenbutton);
+
+function banana () {
+    //alert ("You CAlled");
+    greenbutton.innerHTML="I got clicked";
+ 
+}
+
